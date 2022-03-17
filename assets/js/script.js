73e6f5f7c260b27c48298b8545b727b2957188b3
Caleeeb <daycinematography@gmail.com>
2022-03-16 23:16:13 -0400

    app accepts custom values from form elements and validates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createFormHandler = function(event) {
+var taskFormHandler = function(event) {
 
   event.preventDefault();
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
 
   //package up data as an object
   var taskDataObj = {
@@ -38,4 +46,4 @@ var createTaskEl = function(taskDataObj) {
 
 }
 
-formEl.addEventListener("submit", createFormHandler);
+formEl.addEventListener("submit", taskFormHandler);
