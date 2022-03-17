b66dd877ff9bd2b623dc2b72b68b02102cfbb6a5
Caleeeb <daycinematography@gmail.com>
2022-03-16 21:15:49 -0400

    tasks submit correctly and have sub-text beneath them

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,25 @@ var createTaskHandler = function(event) {
 
   event.preventDefault();
 
-  // create a new task item
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
+  // create a new list item
   var listItemEl = document.createElement("li");
-  // style the new task item
   listItemEl.className = "task-item";
-  // add the text
-  listItemEl.textContent = "This is a new task.";
-  // append this element to the task list
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-  // follow these comments above when dynamically creating element with the DOM.
 };
 
 formEl.addEventListener("submit", createTaskHandler);
